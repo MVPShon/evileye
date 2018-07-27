0e5e48e242ed8dce2bdddce90483e6ef61d2b80d
MVPShon <36894232+MVPShon@users.noreply.github.com>
2018-07-27 09:56:22 -0400

    Update evileye.js

--- evileye.js
+++ evileye.js
@@ -277,5 +277,21 @@ evileye.on("message", async message => {
               message.channel.send("I have muted this user!");
             
               }
+    if (message.content.startsWith(prefix + "umute")) {
+        if(message.author.bot) return;
+    if(!message.member.hasPermission("MUTE_MEMBERS")) return message.channel.send("You do not have the right permissions for this.");
+    
+              let toMute = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+              if(!toMute) return message.channel.send("Please specify someone.")
+    
+              let role = message.guild.roles.find(r => r.name == "Muted");
+    
+                if(!role || !toMute.roles.has(role.id)) return message.channel.send("This user is not muted.");
+                
+                await toMute.removeRole(role);
+                message.channel.send("This user is now unmuted");
+    
+                return;
+          }
 })
 evileye.login(process.env.BOT_TOKEN);
