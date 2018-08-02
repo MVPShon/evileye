8decbb11d21cc25b1ac6355d7e7fc582ccc1b2b4
MVPShon <36894232+MVPShon@users.noreply.github.com>
2018-08-02 14:31:28 -0400

    Update evileye.js

--- evileye.js
+++ evileye.js
@@ -91,7 +91,7 @@ evileye.on("message", async message => {
             .addField("Uptime", uptime)
             .addField("Ping", new Date().getTime() - message.createdTimestamp + " ms.")
             .addField("Description", "Multi-functional, ever growing bot based off of the character `Evileye` from the Overlord anime series. Constantly being updated daily with more cool, exciting stuff for you to enjoy!")
-            .addField("Contact", "Discord Server - https://discord.gg/tT8aZjJ")
+            .addField("Important Links", "Discord Server - https://discord.gg/tT8aZjJ | Upvote Evileye - https://discordbots.org/bot/472335600477798420/vote")
             .setThumbnail(evileye.user.displayAvatarURL)
         message.channel.send(embed);
     }
