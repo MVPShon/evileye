cc27d12c7482d6c2807c2027b6dc63305b34e32c
MVPShon <36894232+MVPShon@users.noreply.github.com>
2018-08-03 14:43:01 -0400

    Update evileye.js

--- evileye.js
+++ evileye.js
@@ -68,9 +68,9 @@ evileye.on("message", async message => {
             .setColor(0xff6464)
             .setThumbnail(evileye.user.displayAvatarURL)
             .addField("4k", "Searches for some 4k wallpapers.")
-            .addField("Ass", "Searches for booty pics. *Also works with .booty and .butt*")
+            .addField("Ass", "Searches for booty pics. *Also works with `.booty` and `.butt`*")
             .addField("Hentai", "Pulls a random hentai picture off of Reddit.")
-            .addField("Tits", "Searches for some nice tits. *Also works with boobs and nipples*")
+            .addField("Tits", "Searches for some nice tits. *Also works with `.boobs`*")
             .addField("Rule34/R34", "Searches https://rule34.xxx for hentai tags. `Usage: " + prefix + "rule34 Evileye` *Note - Spaces are represented as UNDERSCORES on the R34 website*")
         message.author.send(embed)
     }
