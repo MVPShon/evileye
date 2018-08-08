44ca4bbdd374a180de4def5acb087482eb785309
MVPShon <36894232+MVPShon@users.noreply.github.com>
2018-08-08 15:14:13 -0400

    Update evileye.js

--- evileye.js
+++ evileye.js
@@ -15,6 +15,24 @@ evileye.on("guildCreate", guild => {
 evileye.on("guildDelete", guild => {
     evileye.users.get("168865955940794368").send("`" + guild.owner.user.username + "` has just removed me from their server: `" + guild.name + "`");
 });
+evileye.on("guildMemberAdd", member => {
+    let welcmsgs = [
+        `${member}, be ready to praise Lord Ainz, foolish human.`,
+        `We usually do not allow your kind into this domain, ${member}.`,
+        `Consider yourself lucky, ${member}, that Lord Ainz has permitted you to be here.`,
+        `${member} has been admitted into Nazarick!`,
+        `${member}, mere mortals like you cannot fathom how lucky you are to be here`,
+        `Lord Ainz expects great things from you, ${member}.`,
+        `${member} should make fine food for the demons.`
+    ]
+        let randwelc = Math.floor((Math.random() * welcmsgs.length));
+    
+    if(member.guild.id !== "466436909607157770") return;
+    evileye.channels.get("474651288517345280").send(`${welcmsgs[randwelc]}`)
+    let pleb = member.guild.roles.find(r => r.name == "Plebs");
+    member.addRole(pleb);
+    });
+
 evileye.on("message", async message => {
     let messageArray = message.content.split(" ");
     let command = messageArray[0];
