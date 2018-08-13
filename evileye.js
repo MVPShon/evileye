b0906f35e4ad54695f920fa6b20813ecf0618274
MVPShon <36894232+MVPShon@users.noreply.github.com>
2018-08-13 15:10:03 -0400

    Update evileye.js

--- evileye.js
+++ evileye.js
@@ -29,61 +29,6 @@ evileye.on("guildCreate", guild => {
 evileye.on("guildDelete", guild => {
     evileye.users.get("168865955940794368").send("`" + guild.owner.user.username + "` has just removed me from their server: `" + guild.name + "`");
 });
-evileye.on("guildMemberRemove", member => {
-    let kickgifs = [
-        "http://1.bp.blogspot.com/-_iwR-5ML5HM/UTds3RJk3hI/AAAAAAAAFQQ/0ihxGgwCazw/s1600/KO.gif",
-        "https://media1.giphy.com/media/l3V0j3ytFyGHqiV7W/giphy.gif",
-        "https://media.giphy.com/media/wOly8pa4s4W88/giphy.gif",
-        "https://i.kym-cdn.com/photos/images/newsfeed/001/054/543/c22.gif"
-    ]
-
-    let serverid = auditlogs[member.guild.id];
-    if (!alogchan) return message.reply(`Please set an auditlog channel using ${prefix}settings auditlogchannel <Channel ID>`);
-    let alogchan = auditlogs[member.guild.id].auditchannelid;
-    if (!serverid) return message.reply(`Please set an auditlog channel using ${prefix}settings auditlogchannel <Channel ID>`);
-    var kickmessage = `${member.user.username} has been kicked from ${member.guild.name}.`
-    let randkick = Math.floor((Math.random() * kickgifs.length));
-    let embed = new Discord.RichEmbed()
-        .setColor(0xff6464)
-        .addField("KICKED", kickmessage)
-        .setFooter("Time of Kick: ")
-        .setTimestamp()
-        .setImage(`${kickgifs[randkick]}`)
-    return evileye.channels.get(alogchan).send(embed)
-})
-       evileye.on("guildBanAdd", message => {
-           let bangifs = [
-                   "https://media1.tenor.com/images/66b9e27c779a1a314f0a8b31bb5609f7/tenor.gif?itemid=6072837",
-                   "http://gifimage.net/wp-content/uploads/2017/07/banned-gif.gif",
-                   "https://media1.tenor.com/images/d588d295136d07891a79ecc4d38e1a22/tenor.gif?itemid=10026092",
-                  "https://media1.tenor.com/images/88bf753468fad45c30be74c6f4747453/tenor.gif?itemid=8540518",
-                  "http://gifimage.net/wp-content/uploads/2017/07/ban-hammer-gif-5.gif",
-              ]
-              let randban = Math.floor((Math.random() * bangifs.length));
-
-          let serverid = auditlogs[message.guild.id];
-          let alogchan = auditlogs[message.guild.id].auditchannelid;
-         if (!serverid) message.reply(`Please set a channel using ${prefix}settings auditlogchannel <Channel ID>`);
-         var banmessage = `${member.user.username} has been banned from ${member.guild.name}.`
-         let embed = new Discord.RichEmbed()
-         .setColor(0xff6464)
-        .addField("BANNED", banmessage)
-         .setFooter("Time of Ban: ")
-         .setTimestamp()
-          .setImage(`${bangifs[randban]}`)
-         evileye.channels.get(alogchan).send(embed)
-          })
-evileye.on("messageDelete", message => {
-    let serverid = auditlogs[message.guild.id];
-    let alogchan = auditlogs[message.guild.id].auditchannelid;
-    let embed = new Discord.RichEmbed()
-    .setColor(0xff6464)
-    .setAuthor("DELETED MESSAGE", message.author.displayAvatarURL)
-    .setDescription("\""+message+ "\" deleted in: "+ message.channel)
-    .setFooter(message.author.username + " deleted at ")
-    .setTimestamp()
-    evileye.channels.get(alogchan).send(embed)
-})
 evileye.on("guildMemberAdd", member => {
     let welcmsgs = [
         `${member}, be ready to praise Lord Ainz, foolish human.`,
@@ -151,7 +96,6 @@ evileye.on("message", async message => {
             .addField("Kick", "Kicks someone from your server. `Usage: " + prefix + "kick Evileye`")
             .addField("Mute", "Creates a muted role and mutes the target. `Usage: " + prefix + "mute Evileye`")
             .addField("Purge/Delete", "Purges/deletes up to 100 messages from chat. `Usage: " + prefix + "purge 50`")
-            .addField("Settings", "Check all the current server management commands.`Usage: " + prefix + "settings")
             .addField("Unmute", "Unmutes the target. `Usage: " + prefix + "unmute Evileye`")
         message.channel.send(embed)
     }
@@ -570,30 +514,6 @@ evileye.on("message", async message => {
                 .catch(error => message.reply("There was a problem making this role. Make sure you're using the right colors."))
         } else {
             message.reply(`There's already a role with the name "${role.name}".`)
-        }
-    }
-        if (message.content.startsWith(prefix + "settings")) {
-        if (!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("You do not have the right permissions for this.");
-        if (!args[0]) {
-            let embed = new Discord.RichEmbed()
-                .setTitle("Server Settings")
-                .setColor(0xff6464)
-                .addField("Audit Log", "Set your channel's audit log with this command. `Usage: " + prefix + "settings auditlogchannel (Channel ID Here)`")
-                .addField("Prefix", "Set your channel's custom prefix with this command. `Usage: " + prefix + "settings prefix (prefix)`")
-                .setThumbnail(evileye.user.displayAvatarURL)
-            message.channel.send(embed)
-        }
-        if (args[0] === "auditlogchannel" || args[0] === "auditlog") {
-            if (!args[1]) return message.reply("Please specify a channel ID! To find your channel ID type `\\#channelname`. Then type `" + prefix + "settings auditlogchannel (ID NUMBER)`. Note - You do not need the <, # or the > from the ID. Just the numbers.");
-
-            auditlogs[message.guild.id] = {
-                auditchannelid: args[1]
-            }
-
-            fs.writeFile("./auditlogs.json", JSON.stringify(auditlogs, null, 4), err => {
-                if (err) throw err;
-                message.reply(`Set auditlog channel to #${args[1]}`);
-            });
         }
     }
     if (message.content.startsWith(prefix + "mute")) {
