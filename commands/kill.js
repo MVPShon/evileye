a63caec3e1ac4eb5b255b376ebb8137ec8d65407
MVPShon <36894232+MVPShon@users.noreply.github.com>
2018-08-20 12:28:39 -0400

    Update kill.js

--- commands/kill.js
+++ commands/kill.js
@@ -1,5 +1,14 @@
 const Discord = require("discord.js");
 exports.run = async (evileye, message, args) => {
+    if (!args[0]) { 
+        let kills = require("./suicide.json");
+let killmethod = [Math.floor((Math.random() * kills.length))];
+let embed = new Discord.RichEmbed()
+    .setAuthor("| Kill", message.author.displayAvatarURL)
+    .setColor(0xff6464)
+    .setDescription(kills[killmethod].replace(/userkiller/g, message.author.username))
+message.channel.send(embed)
+}
     if (args[0]) {
     if (message.mentions) {
         if (message.mentions.users) {
