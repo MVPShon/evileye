e2db5c8628634d22c1780a7d183a4822afcb493a
MVPShon <36894232+MVPShon@users.noreply.github.com>
2018-08-20 16:47:19 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,5 +6,6 @@ let embed = new Discord.RichEmbed()
 .setColor(0xff6464)
 .addField("Info", "To find basic info on the bot, just type `" + prefix + "info`")
 .addField("Commands", "Type `" + prefix + "commands` for a list of all commands.")
+.setFooter("All commands should be typed in LOWERCASE to get them to work")
 message.channel.send(embed)
 }
