df7514f6b455da9523d58e338cd7ff4a27ca730f
MVPShon <36894232+MVPShon@users.noreply.github.com>
2018-08-31 02:37:22 -0400

    Add files via upload

--- commands/4k.js
+++ commands/4k.js
@@ -1,23 +1,15 @@
 const Discord = require("discord.js");
 exports.run = async (evileye, message, args) => {
-    if (!message.channel.nsfw) return message.reply("🔞 This command can only be used on an NSFW Channel! 🔞")
-    var randomPuppy = require('random-puppy');
-    var subreddits = [
-        'NSFW_Wallpapers',
-        'SexyWallpapers',
-        'HighResNSFW',
-        'nsfw_hd',
-        'UHDnsfw'
-    ]
-    var sub = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
-    randomPuppy(sub)
-        .then(url => {
-            let embed = new Discord.RichEmbed()
-                .setColor(0xff6464)
-                .setFooter("Image doesn't load? Probably a video or a deleted image.")
-                .setImage(url);
-            message.channel.send({
-                embed
-            });
-        })
+if (!message.channel.nsfw) return message.reply("🔞 This command can only be used on an NSFW Channel! 🔞")
+var fourk = require('reddit4k');
+fourk(function(data, err) {
+    var embed = new Discord.RichEmbed()
+        .setTitle(data.title[0])
+        .setColor(0xff6464)
+        .setImage(data.url[0])
+        .setFooter("r/"+data.subreddit[0])
+    message.channel.send({
+        embed
+    });
+});
 }
