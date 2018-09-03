eee431d438071fff1c799fa6f11aa6f2df872e88
MVPShon <36894232+MVPShon@users.noreply.github.com>
2018-09-03 19:25:10 -0400

    Create food.js

---
+++ commands/food.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+exports.run = async (evileye, message, args) => {
+        var randomPuppy = require('random-puppy');
+    var subreddits = [
+        'FoodPorn',
+        'Food'
+    ]
+    var sub = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
+    randomPuppy(sub)
+        .then(url => {
+            let embed = new Discord.RichEmbed()
+                .setImage(url);
+            message.channel.send({
+                embed
+            });
+        })
+    message.channel.send(embed)
+}
