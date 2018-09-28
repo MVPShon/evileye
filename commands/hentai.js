ce18f9651061f50e3405d74913aee3c20a20804e
MVPShon <36894232+MVPShon@users.noreply.github.com>
2018-09-28 14:03:28 -0400

    Update hentai.js

--- commands/hentai.js
+++ commands/hentai.js
@@ -3,7 +3,6 @@ exports.run = async (evileye, message, args) => {
 if (!message.channel.nsfw) return message.reply("🔞 This command can only be used on an NSFW Channel! 🔞")
 var hentaipic = require('hentaipic');
 hentaipic(function(data, err) {
-    var embed = new Discord.RichEmbed()
 message.channel.send(`**${data.title[0]}** -- r/${data.subreddit[0]}\n${data.url[0]}`);
 });
 }
