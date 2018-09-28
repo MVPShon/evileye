9e3d41d37cbe4867011dc9dc856a5d6307f8fd39
MVPShon <36894232+MVPShon@users.noreply.github.com>
2018-09-27 23:22:11 -0400

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -2,6 +2,6 @@ const Discord = require("discord.js");
 exports.run = async (evileye, message, args) => {
     var redditmeme = require('reddit-memes');
 redditmeme(function(data, err) {
-    message.channel.send(`**${data.title[0]} -- ${data.subreddit[0]}**\n${data.url[0]}`);
+    message.channel.send(`**${data.title[0]}** -- r/${data.subreddit[0]}\n${data.url[0]}`);
 });
 }
