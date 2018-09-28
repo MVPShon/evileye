1d6f9c653ef3b2618d56293b854c502e0eaa74e5
MVPShon <36894232+MVPShon@users.noreply.github.com>
2018-09-27 23:12:42 -0400

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -2,12 +2,6 @@ const Discord = require("discord.js");
 exports.run = async (evileye, message, args) => {
     var redditmeme = require('reddit-memes');
 redditmeme(function(data, err) {
-    var embed = new Discord.RichEmbed()
-        .setTitle(data.title[0])
-        .setColor(0xff6464)
-        .setImage(data.url[0])
-    message.channel.send({
-        embed
-    });
+    message.channel.send(`*${data.title[0]*}\n${data.url[0]}`);
 });
 }
