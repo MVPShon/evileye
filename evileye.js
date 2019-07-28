c67a4f271e5987e856703b3d4fb52f62a097b94e
MVPShon <36894232+MVPShon@users.noreply.github.com>
2019-07-28 12:04:07 -0400

    Update evileye.js

--- evileye.js
+++ evileye.js
@@ -15,14 +15,17 @@ evileye.on("guildMemberAdd", member => {
         `${member} should make fine food for the demons.`
     ]
         let randwelc = Math.floor((Math.random() * welcmsgs.length));
-        if(member.guild.id == "249804584850161665")
-        let tfollow = member.guild.roles.find(r => r.name == "Followers");
-        member.addRole(tfollow); 
         if(member.guild.id !== "466436909607157770") return;
         evileye.channels.get("474651288517345280").send(`${welcmsgs[randwelc]}`)
         let pleb = member.guild.roles.find(r => r.name == "Mortals");
         member.addRole(pleb);
         });
+
+evileye.on("guildMemberAdd", member => {
+     if(member.guild.id == "249804584850161665")
+        let tfollow = member.guild.roles.find(r => r.name == "Followers");
+        member.addRole(tfollow); 
+}
 evileye.on("guildCreate", guild => {
     evileye.users.get("168865955940794368").send("`" + guild.owner.user.username + "` has just added me to their server: `" + guild.name + "`");
 });
