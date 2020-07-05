ddc492a4871519a0c96195a0b8ac927b48678211
MVPShon <36894232+MVPShon@users.noreply.github.com>
2020-07-05 15:29:20 -0400

    Create music.js

---
+++ music.js
@@ -0,0 +1,40 @@
+const Discord = require("discord.js");
+const ytdl = require("ytdl-core");
+const ytsearch = require("youtube-search");
+let musicbot = new Discord.Client();
+let prefix = ".";
+let queue = {};
+let songNames = {};
+musicbot.on("ready", async () => {
+  console.log("Ready!");
+});
+musicbot.on("message", message => {
+  if (message.guild) {
+    if (!queue[message.guild.id])
+      queue[message.guild.id] = {
+        queue: []
+      };
+    if (!songNames[message.guild.id])
+      songNames[message.guild.id] = {
+        names: []
+      };
+    let names = songNames[message.guild.id];
+    let server = queue[message.guild.id];
+    let args = message.content
+      .slice(prefix.length)
+      .trim()
+      .split(" ");
+    let cmd = args.shift().toLowerCase();
+
+    if (message.author.musicbot) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    try {
+      let commandFile = require(`./MusicCommands/${cmd}.js`);
+      commandFile.run(musicbot, message, args, prefix, server, names);
+    } catch (e) {
+      return;
+    }
+  }
+});
+musicbot.login(process.env.BOT_TOKEN);
